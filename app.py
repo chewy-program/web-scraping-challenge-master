c00198f19d3fb3c95787615b1a3afd65b2fa219c
Zachary Breen <zgbreen@gmail.com>
2021-12-29 17:04:40 +1100

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ from pymongo import MongoClient
 
 # Create an instance of Flask
 app = Flask(__name__)
-
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017")
+client = MongoClient("mongodb://localhost:27017/")
 
 
 @app.route("/scrape")
@@ -19,8 +17,8 @@ def scrape():
     mars_data = scrape_mars.mars_scrape()
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
-
+    db = client["marsDB"]
+    db.update({}, mars_data, upsert=True)
     # Redirect back to home page
     return redirect("/")
 #client = MongoClient("mongodb://127.0.0.1:27017") #host uri    
@@ -30,8 +28,8 @@ def scrape():
 def home():
     client = MongoClient("mongodb://localhost:27017/")
     db = client["marsDB"]
-    url_data = db.urls
-    hemisphere_data = db["hemispheres"].find_one() 
+    url_data = db["urls"]
+    hemisphere_data = db["hemispheres"].find()
     article_data = db["articles"].find_one() 
     img_link = url_data.find_one()
     # Return template and data
